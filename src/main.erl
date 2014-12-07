7ca213b4d6a5a16b6d6e827048f0b37e40071452
drimtajm <angela.johansson@gmail.com>
2014-12-07 17:04:33 +0000

    Save work in progress

--- src/main.erl
+++ src/main.erl
@@ -10,7 +10,8 @@ go(Options) ->
     Distributed = proplists:get_value(distributed, Options, false),
     DestinationTime = {{2014, 6, 10}, {14, 55, 0}},
     start_servers(DestinationTime, Distributed),
-    case Duration of
+    countdown_clock:set_destination_time(DestinationTime, {distributed, Distributed}),  
+  case Duration of
 	long ->
 	    timer:sleep(30000),
 	    change_destination_time(Distributed),
@@ -24,15 +25,17 @@ go(Options) ->
 start_servers(DestinationTime, Distributed) ->
     {ok, _CDServerPid} =
 	countdown_clock:start_link({distributed, Distributed}),
-    {ok, _DTServerPid} =
-	destination_time:start_link(DestinationTime,
-				    {distributed, Distributed}),
-    {ok, _CTServerPid} = current_time:start_link().
+%%    {ok, _DTServerPid} =
+%%	destination_time:start_link(DestinationTime,
+%%				    {distributed, Distributed}),
+%%    {ok, _CTServerPid} = current_time:start_link(),
+    ok.
 
 stop_servers(Distributed) ->
     countdown_clock:stop({distributed, Distributed}),
-    destination_time:stop(),
-    current_time:stop().
+%%    destination_time:stop(),
+%%    current_time:stop().
+ok.
 
 change_destination_time(Distributed) ->
     %% set destination time to roughly 2 minutes from now
@@ -40,6 +43,7 @@ change_destination_time(Distributed) ->
     {NewHH, NewMM} = if MM > 57 -> {HH+1, MM+2 rem 60};
 			true    -> {HH, MM+2}
 		     end,
-    destination_time:change_destination_time({Date, {NewHH, NewMM, 0}},
+%%    destination_time:change_destination_time
+countdown_clock:set_destination_time({Date, {NewHH, NewMM, 0}},
 					     {distributed, Distributed}),
     ok.
